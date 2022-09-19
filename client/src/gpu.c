7af413fe948fbbcc6690fb6ed8df6c1ed1c75856
Joseph Marlin <j.marlin@outlook.com>
2022-09-18 23:51:59 -0400

    Initial pass at sprite support

--- client/src/gpu.c
+++ client/src/gpu.c
@@ -7,10 +7,10 @@ uint16_t videoAddressToMapIndex(uint16_t addr) {
         (addr >> 3) & 0x001F;
 }
 
-uint8_t tileByteOffsetFromVideoAddressAndAttribute(uint16_t addr, uint8_t attribute) {
+uint8_t tileByteOffsetFromCoordinatesAndAttribute(uint8_t x, uint8_t y, uint8_t attribute) {
 #warning TODO: Add x and y scrolling
-    uint8_t tileXHalf = ((addr & 0x0004) >> 2) ^ (0x01 & ATTR_HMIRRORED(attribute));
-    uint8_t tileYOffset = (((addr >> 3) & 0x0007) ^ (0x07 & ATTR_VMIRRORED(attribute))) << 1;
+    uint8_t tileXHalf = ((x & 0x0004) >> 2) ^ (0x01 & ATTR_HMIRRORED(attribute));
+    uint8_t tileYOffset = ((y & 0x0007) ^ (0x07 & ATTR_VMIRRORED(attribute))) << 1;
 
     return tileYOffset | tileXHalf;
 }
@@ -23,31 +23,81 @@ uint8_t attributeFromTileMapIndex(GPU* gpu, uint16_t tileMapIndex) {
     return (gpu->attr[tileMapIndex >> 1] >> (tileMapIndex & 0x0001)) & 0x0F;
 }
 
-uint8_t tileByteFromTileMapIndexAddressAndAttribute(GPU* gpu, uint16_t tileMapIndex, uint16_t addr, uint8_t attribute) {
-    return gpu->tile[tileBaseOffsetFromTileIndex(gpu, tileMapIndex) + tileByteOffsetFromVideoAddressAndAttribute(addr, attribute)];
+uint8_t tileByteFromTileMapIndexCoordinatesAndAttribute(GPU* gpu, uint16_t tileMapIndex, uint8_t x, uint8_t y, uint8_t attribute) {
+    return gpu->tile[tileBaseOffsetFromTileIndex(gpu, tileMapIndex) + tileByteOffsetFromCoordinatesAndAttribute(x, y, attribute)];
 }
+
 uint8_t tilePixelPaletteIndexFromTileByteAddressAndAttribute(uint8_t tileByte, uint16_t addr, uint8_t attribute) {
     uint8_t tileXSubAddr = (((uint8_t)(addr & 0x0003)) ^ (0x03 & ATTR_HMIRRORED(attribute))) << 1;
 
     return (tileByte >> (6 - tileXSubAddr)) & 0x03;
 }
 
-uint8_t pixelColorFromPaletteIndexAndAttribute(GPU* gpu, uint8_t tilePixelPaletteIndex, uint8_t attribute) {
+uint32_t pixelColorFromPaletteIndexAndAttribute(GPU* gpu, uint8_t tilePixelPaletteIndex, uint8_t attribute) {
     return tilePixelPaletteIndex
         ? gpu->palette[ATTR_PALETTE(attribute)][tilePixelPaletteIndex - 1]
         : gpu->bgColor;
 }
 
-uint8_t tilePixelFromTileAndVideoAddress(GPU* gpu, uint16_t tileMapIndex, uint16_t addr) {
+uint32_t tilePixelIndexFromTileAndVideoAddress(GPU* gpu, uint16_t tileMapIndex, uint16_t addr) {
     uint8_t attribute = attributeFromTileMapIndex(gpu, tileMapIndex);
-    uint8_t tileByte = tileByteFromTileMapIndexAddressAndAttribute(gpu, tileMapIndex, addr, attribute);
-    uint8_t tilePixelPaletteIndex = tilePixelPaletteIndexFromTileByteAddressAndAttribute(tileByte, addr, attribute);
+    uint8_t tileByte = tileByteFromTileMapIndexCoordinatesAndAttribute(gpu, tileMapIndex, VIDEO_ADDR_XPOS(addr), VIDEO_ADDR_YPOS(addr), attribute);
 
-    return pixelColorFromPaletteIndexAndAttribute(gpu, tilePixelPaletteIndex, attribute);
+    return tilePixelPaletteIndexFromTileByteAddressAndAttribute(tileByte, addr, attribute);
+}
+
+uint8_t videoAddressToSpriteIndex(GPU* gpu, int* spriteFound, uint16_t addr) {
+    *spriteFound = 1;
+
+    uint8_t xpos = VIDEO_ADDR_XPOS(addr);
+    uint8_t ypos = VIDEO_ADDR_YPOS(addr);
+
+    //TODO: Support multi-tile sprites
+    for(uint8_t i = 0; i < 32; i++) if(
+        xpos >= SPRITE_X(gpu->sprites[i]) &&
+        xpos < (SPRITE_X(gpu->sprites[i]) + 8) &&
+        ypos >= SPRITE_Y(gpu->sprites[i]) &&
+        ypos < (SPRITE_Y(gpu->sprites[i]) + 8)
+    ) return i;
+
+    *spriteFound = 0;
+
+    return 0;
+}
+
+uint8_t spritePixelIndexFromIndexAndVideoAddress(GPU* gpu, uint8_t spriteIndex, uint16_t addr) {
+    
+    uint8_t tileByte = tileByteFromTileMapIndexCoordinatesAndAttribute(
+        gpu,
+        SPRITE_TILE_INDEX(gpu->sprites[spriteIndex]),
+        VIDEO_ADDR_XPOS(addr) - SPRITE_X(gpu->sprites[spriteIndex]),
+        VIDEO_ADDR_YPOS(addr) - SPRITE_Y(gpu->sprites[spriteIndex]),
+        SPRITE_ATTR(gpu->sprites[spriteIndex])
+    );
 }
 
 uint32_t lookUpPixel(GPU* gpu, uint16_t addr) {
-    uint16_t tileMapIndex = videoAddressToMapIndex(addr);
+    int spriteFound;
+    uint8_t spriteIndex = videoAddressToSpriteIndex(gpu, &spriteFound, addr);
 
-    return tilePixelColorFromTileAndVideoAddress(gpu, tileMapIndex, addr);
+    uint16_t pixelIndex = spriteFound
+        ? spritePixelIndexFromIndexAndVideoAddress(gpu, spriteIndex, addr)
+        : 0;
+
+    int isSpritePixel = pixelIndex;
+
+    uint16_t tileMapIndex = isSpritePixel
+        ? 0
+        : videoAddressToMapIndex(addr);
+
+    pixelIndex = pixelIndex
+        ? pixelIndex
+        : tilePixelIndexFromTileAndVideoAddress(gpu, tileMapIndex, addr);
+
+    return pixelColorFromIndexAndAttribute(
+        gpu,
+        pixelIndex,
+        isSpritePixel
+            ? SPRITE_ATTR(gpu->sprites[spriteIndex])
+            : attributeFromTileMapIndex(gpu, tileMapIndex));
 }
--- client/src/gpu.h
+++ client/src/gpu.h
@@ -7,17 +7,31 @@
 #define ATTR_HMIRRORED(a) (((a) & 0x04) ? 0xFF : 0x00)
 #define ATTR_VMIRRORED(a) (((a) & 0x08) ? 0xFF : 0x00)
 
+#define SPRITE_X(s) ((uint8_t)(((s) >> 24) & 0xFF))
+#define SPRITE_Y(s) ((uint8_t)(((s) >> 16) & 0xFF))
+#define SPRITE_TILE_INDEX(s) ((uint8_t)(((s) >> 8) & 0xFF))
+#define SPRITE_ATTR(s) ((uint8_t)((s) & 0xFF))
+
+#define VIDEO_ADDR_XPOS(a) ((uint8_t)((a) & 0xFF))
+#define VIDEO_ADDR_YPOS(a) ((uint8_t)(((a) >> 8) & 0XFF))
+
 typedef struct GPU_S {
-    uint8_t map[64 * 60]; // = ;
-    uint8_t attr[32 * 60]; // = ;
-    uint8_t tile[256 * 16]; // = ;
-    uint32_t palette[4][3]; // = ;
-    uint32_t bgColor; // = 0;
+    uint8_t map[64 * 60]; 
+    uint8_t attr[32 * 60]; 
+    uint8_t tile[256 * 16]; 
+    uint32_t palette[4][3]; 
+    unit32_t sprites[32];
+    uint32_t bgColor; 
 } GPU;
 
 uint16_t videoAddressToMapIndex(uint16_t addr);
-uint8_t tilePixelFromTileAndVideoAddress(GPU* gpu, uint16_t tileMapIndex, uint16_t addr);
-uint8_t tileByteOffsetFromVideoAddress(uint16_t addr);
+uint8_t tileByteOffsetFromVideoAddressAndAttribute(uint16_t addr, uint8_t attribute);
+uint16_t tileBaseOffsetFromTileIndex(GPU* gpu, uint16_t tileMapIndex);
+uint8_t attributeFromTileMapIndex(GPU* gpu, uint16_t tileMapIndex);
+uint8_t tileByteFromTileMapIndexAddressAndAttribute(GPU* gpu, uint16_t tileMapIndex, uint16_t addr, uint8_t attribute);
+uint8_t tilePixelPaletteIndexFromTileByteAddressAndAttribute(uint8_t tileByte, uint16_t addr, uint8_t attribute);
+uint32_t pixelColorFromPaletteIndexAndAttribute(GPU* gpu, uint8_t tilePixelPaletteIndex, uint8_t attribute);
+uint32_t tilePixelColorFromTileAndVideoAddress(GPU* gpu, uint16_t tileMapIndex, uint16_t addr);
 uint32_t lookUpPixel(GPU* gpu, uint16_t addr);
 
 #endif //GPU_H
